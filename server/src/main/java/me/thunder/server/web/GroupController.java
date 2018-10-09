52c05e83d0a50917363cf8d3e8c043ff6b9342e1
Shunchao Wang <wang.2799@osu.edu>
2018-10-09 11:28:11 -0400

    WIP GroupController.

---
+++ server/src/main/java/me/thunder/server/web/GroupController.java
@@ -0,0 +1,38 @@
+package me.thunder.server.web;
+
+import me.thunder.server.model.Group;
+import me.thunder.server.model.GroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class GroupController {
+
+    private final Logger logger = LoggerFactory.getLogger(GroupController.class);
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @GetMapping("/groups")
+    Collection<Group> groups() {
+        return groupRepository.findAll();
+    }
+
+    @GetMapping("/group/{id}")
+    ResponseEntity<?> getGroup(@PathVariable Long id) {
+        Optional<Group> group = groupRepository.findById(id);
+        return group.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
