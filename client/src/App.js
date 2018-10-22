f71a067cf5c6d22ab6159ed8c17e4a21e1378ae8
Shunchao Wang <wang.2799@osu.edu>
2018-10-22 07:17:42 -0400

    Working on react front end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
 
 class App extends Component {
-
-  state = {
-    isLoading: true,
-    groups: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('http://localhost:8080/api/groups');
-    const body = await response.json();
-    console.log(body);
-    this.setState({isLoading: false, groups: body});
-  }
-
   render() {
-    
-    const {groups, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/groups' exact={true} component={GroupList}/>
+        </Switch>
+      </Router>
+    )
   }
 }
 
